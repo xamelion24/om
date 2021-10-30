86983c693a553929ca0fb85357f8d15f63c0c9a0
xamelion24 <61357814+xamelion24@users.noreply.github.com>
2021-10-30 13:10:14 +0300

    h-s

---
+++ sql/h2/h-s.sql
@@ -0,0 +1,21 @@
+--создание одной из 5 таблиц и установка внешнего ключа
+CREATE TABLE roles_employee
+(
+    id serial  primary key,
+	employee_id int not null unique,
+	role_id int not null,
+	foreign key (employee_id)
+		references employees (id),
+	foreign key (role_id)
+		references roles (id)
+);
+
+--заполнение таблицы
+insert into roles_employee (id, employee_id, role_id) values (default, 1, 1)
+
+--вывод
+select * from employees;
+select * from salary;
+select * from employee_salary;
+select * from roles;
+select * from roles_employee;
